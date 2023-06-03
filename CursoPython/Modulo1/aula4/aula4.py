57c9e3d6ff1690320071c35ab209d71d35147c65
adryanAlbuquerque <adryanfelipe17@hotmail.com>
2023-06-03 01:36:14 -0300

    aula4

---
+++ CursoPython/Modulo1/aula4/aula4.py
@@ -0,0 +1,27 @@
+""""
+Tipos de dados
+
+str - string - 'Assim'
+int - inteiro - 12345 0 -10 -1 -2
+float - real/ponto flutuante - 1.5 -2.5 3.3
+bool - boleando - true/false 10==10
+"""
+
+print(  type('Adryan'),type(123))
+print(  type(1.5), type(20==2))
+print(bool(2==2))
+
+print()
+
+#convertendo
+print("Convertendo")
+print(type(bool("Luiz")),type(int('12')))
+print()
+#Exercicio
+print("Exercicio")
+
+print('Adryan', type('Adryan'))
+print(19, type(19))
+print(1.78, type(1.78))
+print(19>18, type(19>18))
+
