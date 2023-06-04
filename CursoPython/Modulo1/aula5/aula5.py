ad6a8c6e5f9d811ded4ae731e2f1c805fb687f30
adryanAlbuquerque <adryanfelipe17@hotmail.com>
2023-06-04 16:18:47 -0300

    aula5

---
+++ CursoPython/Modulo1/aula5/aula5.py
@@ -0,0 +1,18 @@
+""""
+Operadores Aritiméticos:
+
++(Adição)   -(Subtrair)   *(Multiplicar)   ()(Parenteses)
+
+/(Dividir)  //(Divisão Inteira) **(Exponenciação)
+"""
+
+print('"+" Adição 10+1 = ',10+1)
+print('"-" Subtração 10-1 = ',10-1)
+print('"*" Multiplicação 10*2 = ', 10*2)
+print('"/" Divisão 10/2 = ', 10/2)
+print('"*" Também serve para repetir strings "Olá" *2 = ', 'Olá ' * 2 )
+print('"+" Também serve para concatenar strings  "O" + "lá" = ','O'+'lá')
+print('"//" vai arredondar para inteiro 7 // 2 = ', 7 // 2)
+print('"%" vai mostrar o resto de uma divisão 10 % 2 = ',10%3)
+print('"**" um numero elevado á outro  2**10 = ', 2**10)
+print('"()" Prioriza o que está dentro dos parenteses  5+2*10 que daria 25, vai dar =',(5+2)*10)
