eeab7bb262cffb008e7267248466048349894100
adryanAlbuquerque <adryanfelipe17@hotmail.com>
2023-06-04 18:34:13 -0300

    variaveis

---
+++ CursoPython/Modulo1/aula6/aula6.py
@@ -0,0 +1,14 @@
+"""
+variaveis podem iniciar com letra, ter numeros, separar '_', letra minuscula
+
+"""
+
+nome = 'Adan'
+idade = 19
+altura = 1.70
+e_maior = idade > 18
+peso = 75
+
+imc = peso / altura**2
+
+print("O indice de massa corporal é:", imc)
