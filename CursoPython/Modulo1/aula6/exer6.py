888b91fe91c30a4539160a90c4eba59cfb684148
adryanAlbuquerque <adryanfelipe17@hotmail.com>
2023-06-05 23:52:30 -0300

    exercicio aula 6

---
+++ CursoPython/Modulo1/aula6/exer6.py
@@ -0,0 +1,10 @@
+nome = 'Luiz'
+idade = 19
+peso = 68
+altura = 1.70
+ano = 2023
+imc = peso / altura**2
+
+print(f'{nome} tem {idade} anos, {altura:.2f} de altura e pesa {peso}Kg.')
+print(f'O IMC de {nome} é {imc:.2f}')
+print(f'{nome} nasceu em {ano - idade}')
