0e6f794fa4e9c6b766c804d93ec2f5f291d75e9d
adryanAlbuquerque <adryanfelipe17@hotmail.com>
2023-06-10 21:18:39 -0300

    len

--- CursoPython/Modulo1/aula8/aula8.py
+++ CursoPython/Modulo1/aula8/aula8.py
@@ -5,7 +5,12 @@ Condições   IF - ELIF - ELSE
 nome = input("Qual seu nome? ")
 idade = int(input("Quantos anos você tem? "))
 
+quant_letras = len(nome)
+
 if idade >= 20 and idade <= 30:
     print(f"{nome} pode fazer emprestimo!")
 else:
     print(f"{nome} não está liberado pra fazer emprestimo")
+
+if len(nome) > 0:
+    print(f'{nome} tem {quant_letras}  letras')
