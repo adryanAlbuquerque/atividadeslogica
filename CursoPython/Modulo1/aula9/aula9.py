7e797dc547d9356a6be24e2a43d23ffba8a33b1d
adryanAlbuquerque <adryanfelipe17@hotmail.com>
2023-06-11 22:24:36 -0300

    aula9

--- CursoPython/Modulo1/aula9/aula9.py
+++ CursoPython/Modulo1/aula9/aula9.py
@@ -1,10 +1,25 @@
-a = ''
-b = 5
+nome_cad = input("Cadastre seu nome: ")
+senha_cad = int(input("Cadastre sua senha: "))
+print("Login")
+login_nome = input("Nome:")
+login_senha= int(input("Senha:"))
 
 """if not b > a:
     print(f'{b}(b) não é maior que {a}(a)')
 else:
     print(f'{b}(b) é maior que {a}(a)')"""
 
-if not a:
-    print("a não tem valor")
+'''if not a:
+    print("a não tem valor")'''
+
+"""if 'jo'in nome:
+    print(f"Acesso liberado {nome}")
+else:
+    print(f'{nome} você não está cadastrado, acesso negado!')"""
+
+if nome_cad == login_nome and senha_cad == login_senha:
+    print("Login Efetuado com Sucesso!")
+
+else:
+    print("Usuário ou Senha inválidos!")
+
