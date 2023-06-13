3d30919f9d40fc74107fe2457537f721555fbe08
adryanAlbuquerque <adryanfelipe17@hotmail.com>
2023-06-12 23:21:43 -0300

    aula10

--- CursoPython/Modulo1/aula10/aula10.py
+++ CursoPython/Modulo1/aula10/aula10.py
@@ -5,3 +5,8 @@ if qtd_caracter >= 6:
     print("Usuário cadastrado!")
 else:
     print("Por favor, digite uma senha com mais de 6 caracteres!")
+
+str1 = input("digite uma coisa: ")
+str2 = input("digite outra coisa: ")
+
+print(f"A quantidade de letras é igual a {len(str1) + len(str2)}")
