cfcd2e7b2b153250dc2958f12981983b3d24b81f
adryanAlbuquerque <adryanfelipe17@hotmail.com>
2023-06-23 21:49:01 -0300

    e1

--- CursoPython/Modulo1/aula1/e1.py
+++ CursoPython/Modulo1/aula1/e1.py
@@ -1,10 +1,10 @@
 num = input("Digite a primeira nota: ")
-num2 = input(("Digite a segunda nota: "))
-
+num2 = input("Digite a segunda nota: ")
 
 if num.isdigit() and num2.isdigit():
+    num = float(num)
+    num2 = float(num2)
     media = (num + num2) / 2
     print(f"Sua média é = {media}")
-
 else:
     print("Formato inválido!")
